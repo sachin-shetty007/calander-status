761be322988a08ca4926420d7544b77b126cf0a6
Sachin <so.sachin124@gmail.com>
2024-07-26 21:17:47 +0530

    Uploading calender application code files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CalanderApp from './components/CalanderApp';
+import './components/CalanderApp.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <CalanderApp/>
+    </div>
+  )
+}
+
+export default App
